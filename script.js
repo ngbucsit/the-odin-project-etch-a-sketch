649886ecd40e244497efdcb1e48d36bad3d7f25c
ngbucsit <ngbucsit@gmail.com>
2023-08-31 12:01:36 +0800

    feat: add custom grid sizing

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
+let gridSize = 0;
+const INIT_GRID_SIZE = 16;
+
 const container = document.querySelector("#container");
-for (i = 1; i <= 16; i++) {
-  for (j = 1; j <= 16; j++) {
-    const div = document.createElement("div");
-    div.id = `${i}-${j}`;
-    div.classList.add("box");
-    container.appendChild(div);
-  }
+
+function calcFlexBasis(size) {
+  return 800 / size;
 }
 
-const boxes = document.querySelectorAll(".box");
-
-let isMouseDown = false;
-
-boxes.forEach((box) => {
-  box.addEventListener("mousedown", (e) => {
-    e.preventDefault();
-    isMouseDown = true;
-  });
-  box.addEventListener("mousemove", (e) => {
-    e.preventDefault();
-    if (isMouseDown) {
-      e.target.style["background-color"] = "#000";
+function drawGrid(size) {
+  gridSize = size;
+  container.textContent = "";
+  for (i = 1; i <= gridSize; i++) {
+    for (j = 1; j <= gridSize; j++) {
+      const div = document.createElement("div");
+      div.id = `${i}-${j}`;
+      div.classList.add("box");
+      container.appendChild(div);
+      div.style["flex"] = `1 0 ${calcFlexBasis(gridSize)}px`;
     }
+  }
+  const boxes = document.querySelectorAll(".box");
+
+  let isMouseDown = false;
+
+  boxes.forEach((box) => {
+    box.addEventListener("mousedown", (e) => {
+      e.preventDefault();
+      isMouseDown = true;
+    });
+    box.addEventListener("mousemove", (e) => {
+      e.preventDefault();
+      if (isMouseDown) {
+        e.target.style["background-color"] = "#000";
+      }
+    });
   });
+
+  window.addEventListener("mouseup", (e) => {
+    isMouseDown = false;
+  });
+}
+
+drawGrid(INIT_GRID_SIZE);
+
+const gridSizeButton = document.querySelector("#grid-size-btn");
+gridSizeButton.addEventListener("click", () => {
+  gridSize = +prompt("Enter grid size:", 16);
+  drawGrid(gridSize);
 });
 
-window.addEventListener("mouseup", (e) => {
-  isMouseDown = false;
+const plusButton = document.querySelector("#plus");
+plusButton.addEventListener("click", () => {
+  drawGrid(++gridSize);
+});
+
+const minusButton = document.querySelector("#minus");
+minusButton.addEventListener("click", () => {
+  drawGrid(--gridSize);
 });
